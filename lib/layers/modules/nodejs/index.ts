a98b9916c05c723ee4071676211e031ec65ff35d
DennisMajvall <dennis.majvall@gmail.com>
2022-06-20 10:40:57 +0200

    marshalling and fix global-types

--- lib/layers/modules/nodejs/index.ts
+++ lib/layers/modules/nodejs/index.ts
@@ -1,2 +1,8 @@
+declare global {
+  type File = unknown
+  type Blob = unknown
+}
+
 export { DynamoDBClient, ScanCommand } from '@aws-sdk/client-dynamodb';
+export { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 export * from '@aws-sdk/lib-dynamodb';
